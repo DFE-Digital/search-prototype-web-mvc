9984ccc145fd1d8ca9dd77e050d5cb0db45c68b2
AsiaWi <123170965+AsiaWi@users.noreply.github.com>
2024-08-14 09:48:33 +0100

    replace education phase field to match new data/ update serach-prototype package

--- Dfe.Data.SearchPrototype/Web/Mappers/SearchByKeywordResponseToViewModelMapper.cs
+++ Dfe.Data.SearchPrototype/Web/Mappers/SearchByKeywordResponseToViewModelMapper.cs
@@ -42,13 +42,7 @@ public class SearchByKeywordResponseToViewModelMapper : IMapper<SearchByKeywordR
                         Address3 = establishment.Address.Address3,
                         Postcode = establishment.Address.Postcode
                     },
-                    EducationPhase = new EducationPhaseViewModel()
-                    {
-                        IsPrimary = establishment.EducationPhase.IsPrimary,
-                        IsSecondary = establishment.EducationPhase.IsSecondary,
-                        IsPost16 = establishment.EducationPhase.IsPost16
-                    },
-
+                    PhaseOfEducation = establishment.PhaseOfEducation,
                     EstablishmentType = establishment.EstablishmentType,
                     EstablishmentStatusCode = establishment.EstablishmentStatusCode
                 });
