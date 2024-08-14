74e661e6acb390a1894cb35d870f670741674d96
AsiaWi <123170965+AsiaWi@users.noreply.github.com>
2024-08-14 09:47:03 +0100

    pushed data to azure search ai

---
+++ Dfe.Data.SearchPrototype.Data/DocumentBatchHelpers.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace Dfe.Data.SearchPrototype.Data;
+
+class DocumentBatchHelpers
+{
+    private static readonly HttpClient httpClient = new HttpClient();
+
+    public static async Task SendBatchToSearchService(string serviceName, string apiKey, string indexName, string json)
+{
+    var size = Encoding.UTF8.GetByteCount(json);
+    var requestUri = $"https://{serviceName}.search.windows.net/indexes/{indexName}/docs/index?api-version=2020-06-30";
+    var request = new HttpRequestMessage(HttpMethod.Post, requestUri)
+    {
+        Content = new StringContent(json, Encoding.UTF8, "application/json")
+    };
+    request.Headers.Add("api-key", apiKey);
+
+    try
+    {
+        HttpResponseMessage response = await httpClient.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        string responseContent = await response.Content.ReadAsStringAsync();
+        Console.WriteLine("Documents uploaded successfully.");
+        Console.WriteLine(responseContent);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Failed to upload documents: {ex.Message}");
+        //check payload size as Azure Search accepts either 1000docs or 16MB whatever comes first
+        Console.WriteLine($"Payload size: {Encoding.UTF8.GetByteCount(json)} bytes");
+    }
+}
+
+    public static IEnumerable<IEnumerable<dynamic>> SplitDataIntoBatches(IEnumerable<dynamic> source, int batchSize)
+    {
+        var list = source.ToList();
+        for (int i = 0; i < list.Count; i += batchSize)
+        {
+            yield return list.Skip(i).Take(batchSize);
+        }
+    }
+
+}
---
+++ Dfe.Data.SearchPrototype.Data/Program.cs
@@ -0,0 +1,75 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Newtonsoft.Json;
+using System.Globalization;
+using Dfe.Data.SearchPrototype.Data;
+using Dfe.Data.SearchPrototype.Infrastructure.Options;
+using Dfe.Data.Common.Infrastructure.CognitiveSearch.SearchByKeyword.Options;
+
+class Program
+{
+
+    private const int BatchSize = 1000;//max payload is 1000docs or 16MB whatever comes first
+
+    static async Task Main(string[] args)
+    {
+        //var run = new Program();
+        //await run.MethodName();
+        //Azure Cognitive Search service details
+        string serviceName = "s123d01-aisearch";
+        string apiKey = "";
+        string indexName = "test-index";
+        string filePath = @"C:\SearchPrototypeData\GIASextract\edubasealldata20240807.csv";
+        var records = new List<dynamic>();
+
+        using (var reader = new StreamReader(filePath))
+        using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = true, // Assumes the CSV has headers
+            TrimOptions = TrimOptions.Trim, // Trims whitespace
+            BadDataFound = null // Ignores bad data
+        }))
+        {
+            // Reading records as dynamic objects
+            while (csv.Read())
+            {
+                var record = csv.GetRecord<dynamic>();
+                records.Add(record);
+            }
+
+            // Split records into batches and process each batch
+            var batches = DocumentBatchHelpers.SplitDataIntoBatches(records, BatchSize);
+
+            foreach (var batch in batches)
+            {
+                var jsonDocuments = new
+                {
+                    value = batch.Select(record => new
+                    {
+                        id = record.URN,
+                        ESTABLISHMENTNAME = record.EstablishmentName,
+                        TOWN = record.Town,
+                        TYPEOFESTABLISHMENTNAME = record.TypeOfEstablishmentName,
+                        ISPRIMARY = "",
+                        ISSECONDARY = "",
+                        ISPOST16 = "",
+                        STREET = record.Street,
+                        LOCALITY = record.Locality,
+                        ADDRESS3 = record.Address3,
+                        POSTCODE = record.Postcode,
+                        ESTABLISHMENTSTATUSCODE = record.EstablishmentStatusCode
+                    })
+                };
+
+                // Convert the records to JSON
+                string json = JsonConvert.SerializeObject(jsonDocuments, Formatting.Indented);
+
+                await DocumentBatchHelpers.SendBatchToSearchService(serviceName, apiKey, indexName, json);
+            }
+
+        }
+    }
+    }
+
+
+    
