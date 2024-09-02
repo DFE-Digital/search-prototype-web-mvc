c997b73d8a9e247f8b6635a2edefacde732cb2c3
CLAWLOR <catherine.lawlor@education.gov.uk>
2024-09-02 17:53:18 +0100

    test for mapper

---
+++ Dfe.Data.SearchPrototype/Web/Models/FacetValue.cs
@@ -0,0 +1,7 @@
+﻿namespace Dfe.Data.SearchPrototype.Web.Models;
+
+public record FacetValue(
+    string Value,       // e.g., "Open, but proposed to close"
+    int Count,          // e.g., 150
+    bool IsSelected     // e.g., true
+);
--- Dfe.Data.SearchPrototype/Web/Models/SearchResultsViewModel.cs
+++ Dfe.Data.SearchPrototype/Web/Models/SearchResultsViewModel.cs
@@ -8,29 +8,29 @@ public class SearchResultsViewModel
     /// <summary>
     /// View model representation of the available facets.
     /// </summary> 
-    // public List<Facet>? Facets { get; set; } = new();
-    public List<Facet>? Facets { get; set; } = new()
-    {
-        new Facet("Establishment status", new FacetValue[]
-        {
-            new("Open, but proposed to close", 150, true),
-            new("Open", 2543, true),
-            new("Closed", 2001, false),
-            new("Proposed to open", 73, true),
-            new("Proposed to close", 25, false),
-            new("Closed by the department", 3, false)
-        }),
-        new Facet("Type of establishment", new FacetValue[]
-        {
-            new("Academy converter", 42, true),
-            new("Academy sponsor led", 14, true),
-            new("Academy special converter", 6263, false),
-            new("Academy special sponsor led", 2555, true),
-            new("Academy special", 2551, false),
-            new("Academy 16-19 sponsor led", 773, false)
-        }),
-    };
-    
+    public List<Facet>? Facets { get; set; } = new();
+    //public List<Facet>? Facets { get; set; } = new()
+    //{
+    //    new Facet("Establishment status", new FacetValue[]
+    //    {
+    //        new("Open, but proposed to close", 150, true),
+    //        new("Open", 2543, true),
+    //        new("Closed", 2001, false),
+    //        new("Proposed to open", 73, true),
+    //        new("Proposed to close", 25, false),
+    //        new("Closed by the department", 3, false)
+    //    }),
+    //    new Facet("Type of establishment", new FacetValue[]
+    //    {
+    //        new("Academy converter", 42, true),
+    //        new("Academy sponsor led", 14, true),
+    //        new("Academy special converter", 6263, false),
+    //        new("Academy special sponsor led", 2555, true),
+    //        new("Academy special", 2551, false),
+    //        new("Academy 16-19 sponsor led", 773, false)
+    //    }),
+    //};
+
     /// <summary>
     /// View model representation of aggregated search results.
     /// </summary>
@@ -47,16 +47,4 @@ public class SearchResultsViewModel
     /// Property determining the number of search results.
     /// </summary>
     public int SearchResultsCount => SearchItems?.Count ?? 0;
-    
-    
-    public record Facet(
-        string Name, // e.g., "Establishment status"
-        FacetValue[] Values
-    );
-
-    public record FacetValue(
-        string Value,       // e.g., "Open, but proposed to close"
-        int Count,          // e.g., 150
-        bool IsSelected     // e.g., true
-    );
 }
