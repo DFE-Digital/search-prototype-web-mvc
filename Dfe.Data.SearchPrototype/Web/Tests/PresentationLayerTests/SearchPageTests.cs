e794ba069bd5030a1b2c2e36372e5b533544ef94
Antony OAKES <Antony.OAKES@education.gov.uk>
2024-09-11 09:02:52 +0100

    pr comments update

--- Dfe.Data.SearchPrototype/Web/Tests/PresentationLayerTests/SearchPageTests.cs
+++ Dfe.Data.SearchPrototype/Web/Tests/PresentationLayerTests/SearchPageTests.cs
@@ -2,10 +2,9 @@
 using AngleSharp.Html.Dom;
 using Dfe.Data.SearchPrototype.Common.CleanArchitecture.Application.UseCase;
 using Dfe.Data.SearchPrototype.SearchForEstablishments;
-using Dfe.Data.SearchPrototype.Web.Tests.PageObjectModel;
-using Dfe.Data.SearchPrototype.Web.Tests.Pages.Search;
 using Dfe.Data.SearchPrototype.Web.Tests.Shared;
 using Dfe.Data.SearchPrototype.Web.Tests.Shared.Helpers;
+using Dfe.Data.SearchPrototype.Web.Tests.Shared.Pages;
 using Dfe.Data.SearchPrototype.Web.Tests.Shared.TestDoubles;
 using FluentAssertions;
 using Microsoft.AspNetCore.Hosting;
@@ -50,9 +49,9 @@ public class SearchPageTests : IClassFixture<WebApplicationFactory<Program>>
 
         var resultsPage = await HtmlHelpers.GetDocumentAsync(formResponse);
 
-        resultsPage.QuerySelector(SearchResultsPage.SearchResultsNumber.Criteria)!
+        resultsPage.QuerySelector(SearchPage.SearchResultsNumber.Criteria)!
             .TextContent.Should().Contain("Results");
-        resultsPage.GetMultipleElements(SearchResultsPage.SearchResultLinks.Criteria)
+        resultsPage.GetMultipleElements(SearchPage.SearchResultLinks.Criteria)
             .Count().Should().Be(useCaseResponse.EstablishmentResults.Count);
     }
 
