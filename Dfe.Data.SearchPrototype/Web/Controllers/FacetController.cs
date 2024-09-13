ff554da87b03b39cc013ab80abd1bde11f139b9a
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2024-09-13 11:42:50 +0100

    Got must test facets working

--- Dfe.Data.SearchPrototype/Web/Controllers/FacetController.cs
+++ Dfe.Data.SearchPrototype/Web/Controllers/FacetController.cs
@@ -14,18 +14,28 @@ public class FacetController : Controller
 
     public IActionResult Index()
     {
-        List<TestFacet> testFacets =
-        [
-            new(){ Name = "FACET_1", IsSelected = false },
-            new(){ Name = "FACET_2", IsSelected = false },
-            new(){ Name = "FACET_3", IsSelected = false },
-            new(){ Name = "FACET_4", IsSelected = false }
-        ];
-
-        FacetsViewModel model = new()
-        {
-            TestFacets = testFacets
-        };
+        FacetsViewModel model = new() { TheTestFacets = [
+            new TestFacet()
+            {
+                Name = "THE_MAIN_TEST_FACET_1",
+                FacetValues = [
+                    new(){ Value = "FACET_1", IsSelected = false },
+                    new(){ Value = "FACET_2", IsSelected = false },
+                    new(){ Value = "FACET_3", IsSelected = false },
+                    new(){ Value = "FACET_4", IsSelected = false }
+                ]
+            },
+            new TestFacet()
+            {
+                Name = "THE_MAIN_TEST_FACET_2",
+                FacetValues = [
+                    new(){ Value = "FACET_5", IsSelected = false },
+                    new(){ Value = "FACET_6", IsSelected = false },
+                    new(){ Value = "FACET_7", IsSelected = false },
+                    new(){ Value = "FACET_8", IsSelected = false }
+                ]
+            }
+         ]};
 
         return View("Facet", model);
     }
--- Dfe.Data.SearchPrototype/Web/Models/FacetsViewModel.cs
+++ Dfe.Data.SearchPrototype/Web/Models/FacetsViewModel.cs
@@ -2,12 +2,18 @@
 {
     public class FacetsViewModel
     {
-        public List<TestFacet> TestFacets { get; set; }
+        public List<TestFacet> TheTestFacets { get; set; }
     }
 
     public class TestFacet
     {
         public string Name { get; set; }
+        public List<TestFacetValue> FacetValues { get; set; }
+    }
+
+    public class TestFacetValue
+    {
+        public string Value { get; set; }
         public bool IsSelected { get; set; }
     }
 }
