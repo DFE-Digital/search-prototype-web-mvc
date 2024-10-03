4ea2398a40cf594e53ce7118b81408fb9f75af3e
CLAWLOR <catherine.lawlor@education.gov.uk>
2024-10-03 11:29:59 +0100

    remove redundant test

--- Dfe.Data.SearchPrototype/Web/Tests/Unit/Controllers/HomeControllerTests.cs
+++ Dfe.Data.SearchPrototype/Web/Tests/Unit/Controllers/HomeControllerTests.cs
@@ -74,34 +74,6 @@ public class HomeControllerTests
     }
 
     [Fact]
-    public async Task Index_NoSearchKeyword_NullViewModel()
-    {
-        // arrange
-        Mock<ILogger<HomeController>> mockLogger = LoggerTestDouble.MockLogger();
-
-        IUseCase<SearchByKeywordRequest, SearchByKeywordResponse> mockUseCase =
-            new SearchByKeywordUseCaseMockBuilder().Create();
-
-        Mock<ISearchResultsFactory> mockSearchResultsFactory = SearchResultsFactoryTestDouble.MockFor(new Web.Models.ViewModels.SearchResults());
-
-        Mock<IMapper<Dictionary<string, List<string>>?, IList<FilterRequest>>> mockRequestMapper =
-            ViewModelSelectedFacetsToFilterRequestMapperTestDouble.MockFor([]);
-
-        //act
-        HomeController controller =
-            new(mockLogger.Object, mockUseCase,
-                mockSearchResultsFactory.Object,
-                mockRequestMapper.Object);
-
-        IActionResult result = await controller.Index(new SearchRequest());
-
-        // assert
-        result.Should().NotBeNull();
-        ViewResult viewResult = Assert.IsType<ViewResult>(result);
-
-    }
-
-    [Fact]
     public async Task SearchWithFilters_NoSearckKeywordOnSearchRequest_NullViewModel()
     {
         // arrange
@@ -121,19 +93,17 @@ public class HomeControllerTests
         SearchRequest searchRequest =
             new()
             {
-                SearchKeyword = null!,
                 SelectedFacets = new Dictionary<string, List<string>>() {
                     { "Facet_1", ["Facet_1_Value"] }
                 }
             };
 
-        //act
         HomeController controller =
             new(mockLogger.Object, mockUseCase,
                 mockSearchResultsFactory.Object,
                 mockRequestMapper.Object);
 
-        // assert/verify
+        // act
         IActionResult result = await controller.Index(searchRequest);
 
         // assert
