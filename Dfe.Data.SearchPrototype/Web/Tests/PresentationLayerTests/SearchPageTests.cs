de41588506b4ff2372085438c5df1dc37e822962
CLAWLOR <catherine.lawlor@education.gov.uk>
2024-10-10 13:18:53 +0100

    presentation layer tests either check that a form submission sends the correct values to the mocked use-case, or that a use-case response is shown correctly in the view

--- Dfe.Data.SearchPrototype/Web/Tests/PresentationLayerTests/SearchPageTests.cs
+++ Dfe.Data.SearchPrototype/Web/Tests/PresentationLayerTests/SearchPageTests.cs
@@ -21,7 +21,7 @@ namespace Dfe.Data.SearchPrototype.Web.Tests.PresentationLayerTests;
 
 public class SearchPageTests : IClassFixture<WebApplicationFactory<Dfe.Data.SearchPrototype.Web.Program>>
 {
-    private const string uri = "http://localhost";
+    private const string homeUri = "http://localhost";
     private Mock<IUseCase<SearchByKeywordRequest, SearchByKeywordResponse>> _useCase = new();
     private readonly HttpClient _client;
     private readonly IBrowsingContext _context;
@@ -35,20 +35,49 @@ public class SearchPageTests : IClassFixture<WebApplicationFactory<Dfe.Data.Sear
     }
 
     [Fact]
-    public async Task Search_ByKeyword_WithNoEstablishmentResultsAndNoFacets_ShowsNoResultsText()
+    public async Task Search_SendsKeywordAndSelectedFiltersToUsecase()
     {
+        // arrange
+        var searchTerm = "e.g. School name";
+        SearchByKeywordRequest? capturedUsecaseRequest = default;
+
+        var useCaseResponse = SearchByKeywordResponseTestDouble.Create();
+        _useCase.Setup(useCase => useCase.HandleRequest(It.IsAny<SearchByKeywordRequest>()))
+            .Callback<SearchByKeywordRequest>((x) => capturedUsecaseRequest = x)
+            .ReturnsAsync(useCaseResponse);
+
+        // act
+        // navigate to results page with search keyword)
+        var document = await _context.OpenAsync($"{homeUri}?searchKeyword={searchTerm}");
+        // select some filters
+        var checkedBoxes = document.SelectFilters();
+        // submit filtered search
+        IDocument resultsPage = await document.SubmitSearchAsync();
+
+        // assert
+        var usecaseSelectedFacets = capturedUsecaseRequest!
+            .FilterRequests!
+            .SelectMany(request => request
+                .FilterValues
+                .Where(filterValue => checkedBoxes.Select(checkbox => checkbox.Value).Contains(filterValue.ToString()))
+                );
+
+        checkedBoxes.Select(element => element.Value).Should().BeEquivalentTo(usecaseSelectedFacets.Select(facet => facet.ToString()));
+        capturedUsecaseRequest!.SearchKeyword.Should().Be(searchTerm);
+    }
+
+    [Fact]
+    public async Task Search_WithNoEstablishmentResultsAndNoFacets_ShowsNoResultsText()
+    {
+        // arrange
         var useCaseResponse = SearchByKeywordResponseTestDouble.CreateWithNoResults();
         _useCase.Setup(useCase => useCase.HandleRequest(It.IsAny<SearchByKeywordRequest>()))
             .ReturnsAsync(useCaseResponse);
 
-        var document = await _context.OpenAsync(uri);
+        // act
+        var resultsPage = await _context.OpenAsync($"{homeUri}?searchKeyword=anything");
 
-        IHtmlFormElement form = document!.QuerySelector<IHtmlFormElement>(HomePage.SearchForm.Criteria)!;
-        IDocument resultsPage = await form.SubmitAsync(new
-        {
-            searchKeyWord = "anything - I've mocked the response from the use-case regardless of the request"
-        });
-
+        // assert
         resultsPage.QuerySelector(HomePage.SearchNoResultText.Criteria)!
             .TextContent.Should().Contain("Sorry no results found please amend your search criteria");
         resultsPage.QuerySelector(HomePage.SearchResultsNumber.Criteria)!
@@ -59,18 +88,15 @@ public class SearchPageTests : IClassFixture<WebApplicationFactory<Dfe.Data.Sear
     [Fact]
     public async Task Search_ByKeyword_WithSingleEstablishmentResult_Shows1ResultText()
     {
+        // arrange
         var useCaseResponse = SearchByKeywordResponseTestDouble.CreateWithOneResult();
         _useCase.Setup(useCase => useCase.HandleRequest(It.IsAny<SearchByKeywordRequest>()))
             .ReturnsAsync(useCaseResponse);
 
-        var document = await _context.OpenAsync(uri);
+        // act
+        var resultsPage = await _context.OpenAsync($"{homeUri}?searchKeyword=anything");
 
-        IHtmlFormElement form = document!.QuerySelector<IHtmlFormElement>(HomePage.SearchForm.Criteria)!;
-        IDocument resultsPage = await form.SubmitAsync(new
-        {
-            searchKeyWord = "anything - I've mocked the response from the use-case regardless of the request"
-        });
-
+        // assert
         resultsPage.QuerySelector(HomePage.SearchResultsNumber.Criteria)!
             .TextContent.Should().Be("1 Result");
         resultsPage.QuerySelector(HomePage.SearchResultsContainer.Criteria)!
@@ -81,18 +107,15 @@ public class SearchPageTests : IClassFixture<WebApplicationFactory<Dfe.Data.Sear
     [Fact]
     public async Task Search_ByKeyword_WithMultipleEstablishmentResults_ShowsResults()
     {
+        // arrange
         var useCaseResponse = SearchByKeywordResponseTestDouble.Create();
         _useCase.Setup(useCase => useCase.HandleRequest(It.IsAny<SearchByKeywordRequest>()))
             .ReturnsAsync(useCaseResponse);
 
-        var document = await _context.OpenAsync(uri);
+        // act
+        var resultsPage = await _context.OpenAsync($"{homeUri}?searchKeyword=anything");
 
-        IHtmlFormElement form = document!.QuerySelector<IHtmlFormElement>(HomePage.SearchForm.Criteria)!;
-        IDocument resultsPage = await form.SubmitAsync(new
-        {
-            searchKeyWord = "anything - I've mocked the response from the use-case regardless of the request"
-        });
-
+        // assert
         resultsPage.QuerySelector(HomePage.SearchResultsNumber.Criteria)!
             .TextContent.Should().Contain("Results");
         resultsPage.QuerySelector(HomePage.SearchResultsContainer.Criteria)!
@@ -103,18 +126,15 @@ public class SearchPageTests : IClassFixture<WebApplicationFactory<Dfe.Data.Sear
     [Fact]
     public async Task Search_ByKeyword_WithFacetedResults_ShowsFacets()
     {
+        // arrange
         var useCaseResponse = SearchByKeywordResponseTestDouble.Create();
         _useCase.Setup(useCase => useCase.HandleRequest(It.IsAny<SearchByKeywordRequest>()))
             .ReturnsAsync(useCaseResponse);
 
-        var document = await _context.OpenAsync(uri);
+        // act
+        var resultsPage = await _context.OpenAsync($"{homeUri}?searchKeyword=anything");
 
-        IHtmlFormElement form = document!.QuerySelector<IHtmlFormElement>(HomePage.SearchForm.Criteria)!;
-        IDocument resultsPage = await form.SubmitAsync(new
-        {
-            searchKeyWord = "anything - I've mocked the response from the use-case regardless of the request"
-        });
-
+        // assert
         var filtersHeading = resultsPage.QuerySelector(HomePage.FiltersHeading.Criteria);
         filtersHeading.Should().NotBeNull();
         filtersHeading!.TextContent.Should().Be("Filters");
@@ -125,15 +145,13 @@ public class SearchPageTests : IClassFixture<WebApplicationFactory<Dfe.Data.Sear
         foreach (var expectedFacet in expectedFacets)
         {
             // get the page element and its child nodes for each expected facet - this bit could be done with a page model
-            var matchingFacetPageElement = facetContainer!
-                .GetNodes<IHtmlFieldSetElement>()
-                .Single(element => element.Id != null && element.Id == $"FacetName-{expectedFacet.Name}");
-            var facetLegend = matchingFacetPageElement.GetNodes<IHtmlLegendElement>().Single();
-            var facetInputElements = matchingFacetPageElement.GetNodes<IHtmlInputElement>();
+            var matchingFacetPageElement = resultsPage.GetFacet(expectedFacet.Name);
+            var facetLegend = matchingFacetPageElement.GetLegend();
+            var facetInputElements = matchingFacetPageElement.GetCheckBoxes();
 
-           // assert the facet name is on the page
-           facetLegend.TextContent.Trim().Should().Be(expectedFacet.Name);
-           foreach (var expectedFacetValue in expectedFacet.Results)
+            // assert the facet name is on the page
+            facetLegend.TextContent.Trim().Should().Be(expectedFacet.Name);
+            foreach (var expectedFacetValue in expectedFacet.Results)
             {
                 // assert that each expected facet value appears on the page under the correct facet name
                 var matchedFacet = facetInputElements.Single(inputElement => inputElement!.Value == expectedFacetValue.Value);
