040202c0dd5b9ad7960816050b0b54f9c4d798b1
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2024-10-23 10:15:28 +0100

    Added pagination service

---
+++ Dfe.Data.SearchPrototype/Web/Services/PaginationService.cs
@@ -0,0 +1,26 @@
+﻿namespace Dfe.Data.SearchPrototype.Web.Services
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class PaginationService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="recordCount"></param>
+        /// <param name="recordsPerPage"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public int GetTotalNumberOfPages(int totalNumberOfRecords, int recordsPerPage)
+        {
+            if (totalNumberOfRecords == 0)
+                throw new ArgumentException("The record count must be greater than zero.");
+
+            if (recordsPerPage == 0)
+                throw new ArgumentException("The page size must be greater than zero.");
+
+            return totalNumberOfRecords / recordsPerPage + (totalNumberOfRecords % recordsPerPage > 0 ? 1 : 0);
+        }
+    }
+}
---
+++ Dfe.Data.SearchPrototype/Web/Tests/Services/PaginationServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using Dfe.Data.SearchPrototype.Web.Services;
+using Xunit;
+
+namespace Dfe.Data.SearchPrototype.Web.Tests.Services
+{
+    public sealed class PaginationServiceTests
+    {
+        [Fact]
+        public void xxx()
+        {
+            int totalNumberOfPages = new PaginationService().GetTotalNumberOfPages(totalNumberOfRecords: 92, recordsPerPage: 20);
+        }
+    }
+}
