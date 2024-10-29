9fce79d30fda019f8640a97beba3231b27d34653
AsiaWi <123170965+AsiaWi@users.noreply.github.com>
2024-10-29 13:49:05 +0000

    Mapper pagination tests

--- Dfe.Data.SearchPrototype/Web/Tests/Unit/Mappers/PaginationResultsToPaginationViewModelMapperTests.cs
+++ Dfe.Data.SearchPrototype/Web/Tests/Unit/Mappers/PaginationResultsToPaginationViewModelMapperTests.cs
@@ -14,94 +14,75 @@ namespace Dfe.Data.SearchPrototype.Web.Tests.Unit.Mappers
         {
             // arrange
             const int currentPage = 1;
-            const int totalPageCount = 113;
+            const int totalRecordCount = 113;
 
             // act
-            Pagination response = _mapper.MapFrom(input: (currentPage, totalPageCount));
+            Pagination response = _mapper.MapFrom(input: (currentPage, totalRecordCount));
 
             // assert
             response.Should().NotBeNull();
             response.CurrentPageSequence.Should().Equal([1, 2, 3, 4, 5]);
             response.IsFirstPage.Should().BeTrue();
             response.CurrentPageInLowerPagingBoundary.Should().BeTrue();
-            response.CurrentPageOnLowerPagingThreshold.Should().BeTrue();
+            response.CurrentPageInLowerPagingThreshold.Should().BeTrue();
             response.CurrentPageInUpperPagingBoundary.Should().BeFalse();
-            response.CurrentPageOnUpperPagingThreshold.Should().BeFalse();
+            response.CurrentPageInUpperPagingThreshold.Should().BeFalse();
         }
 
         [Theory]
         [InlineData(3, 113, new int[] { 1, 2, 3, 4, 5 })]
         [InlineData(2, 113, new int[] { 1, 2, 3, 4, 5 })]
-        public void MapFrom_WithCurrentPageAsSecondPage(
-            int currentPage, int totalPageCount, int[] expectedPageSequence)
+        public void MapFrom_WithCurrentPageInLowerPageBoundry(
+            int currentPage, int totalRecordCount, int[] expectedPageSequence)
         {
             // act
-            Pagination response = _mapper.MapFrom(input: (currentPage, totalPageCount));
+            Pagination response = _mapper.MapFrom(input: (currentPage, totalRecordCount));
 
             // assert
             response.Should().NotBeNull();
             response.CurrentPageSequence.Should().Equal(expectedPageSequence);
             response.IsFirstPage.Should().BeFalse();
             response.CurrentPageInLowerPagingBoundary.Should().BeTrue();
-            response.CurrentPageOnLowerPagingThreshold.Should().BeTrue();
+            response.CurrentPageInLowerPagingThreshold.Should().BeTrue();
             response.CurrentPageInUpperPagingBoundary.Should().BeFalse();
-            response.CurrentPageOnUpperPagingThreshold.Should().BeFalse();
+            response.CurrentPageInUpperPagingThreshold.Should().BeFalse();
         }
 
         [Theory]
-        [InlineData(111, 113, new int[] { 109, 110, 111, 112, 113 })]
-        [InlineData(110, 113, new int[] { 108, 109, 110, 111, 112 })]
-        public void MapFrom_WithCurrent(
-            int currentPage, int totalPageCount, int[] expectedPageSequence)
+        [InlineData(12, 113, new int[] { 8, 9, 10, 11, 12 })]
+        [InlineData(11, 113, new int[] { 8, 9, 10, 11, 12 })]
+        public void MapFrom_WithCurrentPageInUpperPageBoundry(
+            int currentPage, int totalRecordCount, int[] expectedPageSequence)
         {
             // act
-            Pagination response = _mapper.MapFrom(input: (currentPage, totalPageCount));
+            Pagination response = _mapper.MapFrom(input: (currentPage, totalRecordCount));
             // assert
             response.Should().NotBeNull();
             response.CurrentPageSequence.Should().Equal(expectedPageSequence);
             response.IsFirstPage.Should().BeFalse();
             response.CurrentPageInLowerPagingBoundary.Should().BeFalse();
-            response.CurrentPageOnLowerPagingThreshold.Should().BeFalse();
+            response.CurrentPageInLowerPagingThreshold.Should().BeFalse();
             response.CurrentPageInUpperPagingBoundary.Should().BeTrue();
-            response.CurrentPageOnUpperPagingThreshold.Should().BeTrue();
+            response.CurrentPageInUpperPagingThreshold.Should().BeTrue();
         }
 
         [Theory]
-        [InlineData(113, 113, new int[] {109 ,110 ,111 , 112, 113})]
-        [InlineData(112, 113, new int[] {109, 110, 111, 112, 113})]
-        public void MapFrom_With(
-            int currentPage, int totalPageCount, int[] expectedPageSequence)
+        [InlineData(8, 113, new int[] { 6, 7, 8, 9, 10 })]
+        [InlineData(7, 113, new int[] { 5, 6, 7, 8, 9 })]
+        public void MapFrom_WithCurrentPageBetweenFirstAndLast2Pages(
+            int currentPage, int totalRecordCount, int[] expectedPageSequence)
         {
             // act
-            Pagination response = _mapper.MapFrom(input: (currentPage, totalPageCount));
+            Pagination response = _mapper.MapFrom(input: (currentPage, totalRecordCount));
 
             // assert
             response.Should().NotBeNull();
             response.CurrentPageSequence.Should().Equal(expectedPageSequence);
             response.IsFirstPage.Should().BeFalse();
             response.CurrentPageInLowerPagingBoundary.Should().BeFalse();
-            response.CurrentPageOnLowerPagingThreshold.Should().BeFalse();
-            response.CurrentPageInUpperPagingBoundary.Should().BeTrue();
-            response.CurrentPageOnUpperPagingThreshold.Should().BeTrue();
-        }
-
-        [Theory]
-        [InlineData(4, 113, new int[] { 2, 3, 4, 5, 6 })]
-        [InlineData(5, 113, new int[] { 3, 4, 5, 6, 7 })]
-        public void MapFrom_WithCurrentPageBetweenFirstAndLast2(
-            int currentPage, int totalPageCount, int[] expectedPageSequence)
-        {
-            // act
-            Pagination response = _mapper.MapFrom(input: (currentPage, totalPageCount));
-
-            // assert
-            response.Should().NotBeNull();
-            response.CurrentPageSequence.Should().Equal(expectedPageSequence);
-            response.IsFirstPage.Should().BeFalse();
-            response.CurrentPageInLowerPagingBoundary.Should().BeFalse();
-            response.CurrentPageOnLowerPagingThreshold.Should().BeTrue();
+            response.CurrentPageInLowerPagingThreshold.Should().BeFalse();
             response.CurrentPageInUpperPagingBoundary.Should().BeFalse();
-            response.CurrentPageOnUpperPagingThreshold.Should().BeFalse();
+            response.CurrentPageInUpperPagingThreshold.Should().BeFalse();
         }
     }
 }
