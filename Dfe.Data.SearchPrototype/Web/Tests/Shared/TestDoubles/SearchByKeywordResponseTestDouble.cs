6955be92a360fb4cf099e156436601eb3fdf3df2
AsiaWi <123170965+AsiaWi@users.noreply.github.com>
2024-10-31 11:48:23 +0000

    WIP - EXPAND TEST DOUBLE TO PAGE RESULTS

--- Dfe.Data.SearchPrototype/Web/Tests/Shared/TestDoubles/SearchByKeywordResponseTestDouble.cs
+++ Dfe.Data.SearchPrototype/Web/Tests/Shared/TestDoubles/SearchByKeywordResponseTestDouble.cs
@@ -6,31 +6,50 @@ namespace Dfe.Data.SearchPrototype.Web.Tests.Shared.TestDoubles;
 
 public static class SearchByKeywordResponseTestDouble
 {
-    public static SearchByKeywordResponse Create()
+    public static SearchByKeywordResponse Create() => CreateWith(new Bogus.Faker().Random.Int(11, 66));
+
+    public static SearchByKeywordResponse CreateWith(int establishmentResultsCount, int pageNumber = 0)
     {
         List<Establishment> establishmentResults = new();
-        for (int i = 0; i < new Bogus.Faker().Random.Int(11, 66); i++)
+        Dictionary<int, List<PageSequence>> pageChunks = new();
+        List<PageSequence> currentPageSequences = new();
+        if (pageNumber > 0)
+        {
+            List<int> rawRecordCount = Enumerable.Range(1, establishmentResultsCount).ToList();
+
+            pageChunks = rawRecordCount.Select((x, i) => new PageSequence{RecordNumber = i})
+                .GroupBy(x => x.RecordNumber / 10).ToDictionary(x => x.Key, y => y.ToList());
+        }
+
+        if (pageChunks.Any())
+        {
+            currentPageSequences = pageChunks[pageNumber - 1];
+        }
+
+        int numberOfResultsPerPage = (currentPageSequences.Any()) ? currentPageSequences.Count : 10;
+
+        for (int i = 0; i < numberOfResultsPerPage; i++)
         {
             establishmentResults.Add(EstablishmentTestDouble.Create());
         }
 
         List<EstablishmentFacet> facetResults = new();
 
-        long? totalNumberOfEstablishments = 0;
-
-        for (int i = 0; i < new Bogus.Faker().Random.Int(11, 66); i++)
+        for (int i = 0; i < numberOfResultsPerPage; i++)
         {
             facetResults.Add(EstablishmentFacetTestDouble.Create(i.ToString()));
 
-            totalNumberOfEstablishments += i;
         }
         return new SearchByKeywordResponse(status: SearchResponseStatus.Success)
         {
-            EstablishmentResults = new EstablishmentResults(establishmentResults, totalNumberOfEstablishments),v
+            EstablishmentResults = new EstablishmentResults(establishmentResults, establishmentResultsCount),
             EstablishmentFacetResults = new EstablishmentFacets(facetResults)
         };
     }
-
+    internal class PageSequence
+    {
+        public int RecordNumber { get; set; }
+    }
     public static SearchByKeywordResponse CreateWithOneResult()
     {
         List<Establishment> establishmentResults = new() {
