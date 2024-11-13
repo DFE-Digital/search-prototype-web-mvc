d2bc721a9387fbdc0fdba42da44902f89e196b9c
Aasim AHMED <Aasim.AHMED@EDUCATION.GOV.UK>
2024-11-13 19:40:04 +0000

    setup xunit default project, add internals visible to resolve and check types from di resolution

---
+++ Dfe.Testing.Pages.UnitTests/DependencyInjectionTests.cs
@@ -0,0 +1,10 @@
+namespace Dfe.Testing.Pages.UnitTests;
+
+public class DependencyInjectionTests
+{
+    [Fact]
+    public void Can_Resolve_IDocumentQueryClientAccessor_From_ServiceContainer()
+    {
+        
+    }
+}
