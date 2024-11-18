aa4b949a74b3229d33b9a593a13d1837afad8c16
Aasim AHMED <Aasim.AHMED@EDUCATION.GOV.UK>
2024-11-18 15:00:52 +0000

    make di available

---
+++ Dfe.Data.SearchPrototype/Web/Tests/EndToEndTests/BaseEndToEndTest.cs
@@ -0,0 +1,28 @@
+﻿using Dfe.Data.SearchPrototype.Web.Tests.EndToEndTests.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.Abstractions;
+
+namespace Dfe.Data.SearchPrototype.Web.Tests.EndToEndTests;
+
+public class BaseEndToEndTest : IDisposable
+{
+    private readonly IServiceScope _serviceScope;
+
+    protected BaseEndToEndTest(ITestOutputHelper testOutputHelper)
+    {
+        _serviceScope = Services.Instance.CreateScope();
+        TestOutputHelper = testOutputHelper;
+    }
+
+    protected ITestOutputHelper TestOutputHelper { get; }
+
+    protected T GetTestService<T>()
+        => _serviceScope.ServiceProvider.GetService<T>()
+            ?? throw new ArgumentNullException($"Unable to resolve type {typeof(T)}");
+
+    public void Dispose()
+    {
+        _serviceScope.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
--- Dfe.Data.SearchPrototype/Web/Tests/EndToEndTests/ExampleUITest.cs
+++ Dfe.Data.SearchPrototype/Web/Tests/EndToEndTests/ExampleUITest.cs
@@ -2,113 +2,32 @@
 using Dfe.Data.SearchPrototype.Web.Tests.Shared.Pages.Components;
 using Dfe.Testing.Pages;
 using Dfe.Testing.Pages.DocumentQueryClient.Provider.WebDriver;
-using Dfe.Testing.Pages.DocumentQueryClient.Provider.WebDriver.Internal;
 using Dfe.Testing.Pages.Pages;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Dfe.Data.SearchPrototype.Web.Tests.EndToEndTests;
 
-public sealed class ExampleUITest
+public sealed class ExampleUITest : BaseEndToEndTest
 {
-    private readonly ServiceProvider _serviceProvider;
-
-    public ExampleUITest()
+    public ExampleUITest(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
     {
-        ServiceCollection services = new();
-        services
-            .AddWebDriver()
-            .AddTransient<SearchComponent>()
-            .AddTransient<NavigationBarComponent>()
-            .AddTransient<HomePage>()
-            .AddTransient<SearchResultsComponent>();
-
-        _serviceProvider = services.BuildServiceProvider();
     }
+
+    // TODO option->navigateToUri = true to dictate to the provider if it should create a document or reuse an existing document
+    // internal logic across providers to have a CachedDocumentFactory?
 
     [Fact]
     public async Task TestConsumptionOf_FullQUeryClient_AsyncScope()
     {
-        using var scope = _serviceProvider.CreateAsyncScope();
-        var pageFactory = scope.ServiceProvider.GetRequiredService<IPageFactory>();
-
         HttpRequestMessage request = new()
         {
             RequestUri = new("https://searchprototype.azurewebsites.net/")
         };
 
-        // TODO option->navigateToUri = true to dictate to the provider if it should create a document or reuse an existing document
-        // internal logic across providers to have a CachedDocumentFactory?
-        var homePage = await pageFactory.CreatePageAsync<HomePage>(request);
+        var homePage = await GetTestService<IPageFactory>().CreatePageAsync<HomePage>(request);
         homePage.Search.GetSubheading().Should().Be("Search establishments");
-    }
-
-    [Fact]
-    public async Task SHOULD_FAIL_TestConsumptionOf_FullQUeryClient_AsyncScope()
-    {
-        using var scope = _serviceProvider.CreateAsyncScope();
-        var pageFactory = scope.ServiceProvider.GetRequiredService<IPageFactory>();
-
-        HttpRequestMessage request = new()
-        {
-            RequestUri = new("https://searchprototype.azurewebsites.net/")
-        };
-
-        // TODO option->navigateToUri = true to dictate to the provider if it should create a document or reuse an existing document
-        // internal logic across providers to have a CachedDocumentFactory?
-        var homePage = await pageFactory.CreatePageAsync<HomePage>(request);
-        homePage.Search.GetSubheading().Should().Be("SHOULD FAIL HERE");
-    }
-
-    [Fact]
-    public async Task TestConsumptionOf_DriverInfrastructure_Alone()
-    {
-        using var scope = _serviceProvider.CreateScope();
-        var pageFactory = scope.ServiceProvider.GetRequiredService<IPageFactory>();
-
-        HttpRequestMessage request = new()
-        {
-            RequestUri = new("https://searchprototype.azurewebsites.net/")
-        };
-
-        // TODO option->navigateToUri = true to dictate to the provider if it should create a document or reuse an existing document
-        // internal logic across providers to have a CachedDocumentFactory?
-        var homePage = await pageFactory.CreatePageAsync<HomePage>(request);
-        homePage.Search.GetSubheading().Should().Be("Search establishments");
-    }
-
-    [Fact]
-    public async Task SHOULD_FAIL_TestConsumptionOf_FullQUeryClient_SyncScope()
-    {
-        using var scope = _serviceProvider.CreateScope();
-        var pageFactory = scope.ServiceProvider.GetRequiredService<IPageFactory>();
-
-        HttpRequestMessage request = new()
-        {
-            RequestUri = new("https://searchprototype.azurewebsites.net/")
-        };
-
-        // TODO option->navigateToUri = true to dictate to the provider if it should create a document or reuse an existing document
-        // internal logic across providers to have a CachedDocumentFactory?
-        var homePage = await pageFactory.CreatePageAsync<HomePage>(request);
-        homePage.Search.GetSubheading().Should().Be("SHOULD_FAIL");
-    }
-
-
-    [Fact]
-    public async Task TestConsumption_Of_Navigator()
-    {
-        using var scope = _serviceProvider.CreateScope();
-        var pageFactory = scope.ServiceProvider.GetRequiredService<IPageFactory>();
-        HttpRequestMessage request = new()
-        {
-            RequestUri = new("https://searchprototype.azurewebsites.net/")
-        };
-        var homePage = await pageFactory.CreatePageAsync<HomePage>(request);
-
-        var navigatorProxy = scope.ServiceProvider.GetRequiredService<IApplicationNavigatorAccessor>();
-        await navigatorProxy.Navigator.NavigateToAsync(new Uri("https://searchprototype.azurewebsites.net/?searchKeyWord=Col"));
-        var something = "x";
     }
 }
