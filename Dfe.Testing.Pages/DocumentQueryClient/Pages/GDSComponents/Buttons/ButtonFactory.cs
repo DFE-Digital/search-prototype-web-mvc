ab8800459418139e65f6c4b2a1545ac8ad11bb0d
Aasim AHMED <Aasim.AHMED@EDUCATION.GOV.UK>
2024-11-21 08:19:40 +0000

    move up

--- Dfe.Testing.Pages/DocumentQueryClient/Pages/GDSComponents/Buttons/ButtonFactory.cs
+++ Dfe.Testing.Pages/DocumentQueryClient/Pages/GDSComponents/Buttons/ButtonFactory.cs
@@ -1,8 +1,12 @@
-﻿namespace Dfe.Testing.Pages.DocumentQueryClient.Pages.GDSComponents.Buttons;
+﻿using System.Linq;
+
+namespace Dfe.Testing.Pages.DocumentQueryClient.Pages.GDSComponents.Buttons;
 
 public sealed class ButtonFactory : ComponentFactoryBase<Button>
 {
     internal static IElementSelector Button => new ElementSelector(".govuk-button");
+    internal static IElementSelector SecondaryButtonStyle => new ElementSelector("govuk-button--secondary");
+    internal static IElementSelector WarningButtonStyle => new ElementSelector(".govuk-button--warning");
     public ButtonFactory(IDocumentQueryClientAccessor documentQueryClientAccessor) : base(documentQueryClientAccessor)
     {
     }
@@ -22,9 +26,9 @@ public sealed class ButtonFactory : ComponentFactoryBase<Button>
                 var classStyles = part.GetAttribute("class") ?? string.Empty;
                 return new Button()
                 {
-                    ButtonType =
-                            classStyles.Contains("govuk-button--secondary") ? ButtonStyleType.Secondary :
-                            classStyles.Contains("gov-uk-button--warning") ? ButtonStyleType.Warning : ButtonStyleType.Primary,
+                    ButtonType = 
+                        classStyles.Contains(SecondaryButtonStyle.ToSelector()) ? ButtonStyleType.Secondary :
+                        classStyles.Contains(WarningButtonStyle.ToSelector()) ? ButtonStyleType.Warning : ButtonStyleType.Primary,
                     Text = part.Text?.Trim() ?? string.Empty,
                     TagName = part.TagName,
                     Disabled = part.HasAttribute("disabled"),
